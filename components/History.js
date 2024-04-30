c567f660de35be4df18136bb53e7d7f916bdfac5
gathan from desk <gathan@sch.gr>
2024-04-30 15:34:16 +0300

    Εμφάνιση ιστορικού από μεταφρασμένο πίνακα

--- components/History.js
+++ components/History.js
@@ -40,7 +40,7 @@ class History extends HTMLElement {
         this.shadow.innerHTML = historyContent;
         this.protocolNo = this.attributes.protocolNo.value;
         this.protocolYear = this.attributes.protocolDate.value.split("-")[0]; // ημερομηνία πρωτοκόλλου στην μορφή 2023-06-06
-        this.loadHistory(this.protocolNo);
+        this.loadHistoryTranslated(this.protocolNo);
     }
 
     attributeChangedCallback(name, oldValue, newValue){
@@ -64,6 +64,29 @@ class History extends HTMLElement {
         }
         else{
             const resdec = await res.result;
+            this.shadow.querySelector("#historySpinner").display = "none"; 
+            this.shadow.querySelector("#historyTableTitleBadge").textContent = resdec.length;
+            let html = "";
+            //$("#historyTable").append(html);
+            for (let key1=0;key1<resdec.length;key1++) {
+                html += "<tr><td>"+resdec[key1]+"</td></tr>" ;
+            }
+            this.shadow.querySelector("#historyTable tbody").innerHTML = html;
+        }
+    }
+
+    async loadHistoryTranslated(protocolNo){
+        this.shadow.querySelector("#historyTable tbody").innerHTML = "";
+        this.shadow.querySelector("#historySpinner").display = "inline-block"; 
+        const urlparams = new URLSearchParams({postData: this.protocolNo, currentYear : this.protocolYear})
+
+        const res = await runFetch("/api/getHistoryTranslated.php", "GET", urlparams);
+        if (!res.success){
+            this.shadow.querySelector("#actionStatus").innerHTML = res.msg;
+            this.shadow.querySelector("#historySpinner").display = "none"; 
+        }
+        else{
+            const resdec = await res.result.ssr;
             this.shadow.querySelector("#historySpinner").display = "none"; 
             this.shadow.querySelector("#historyTableTitleBadge").textContent = resdec.length;
             let html = "";
